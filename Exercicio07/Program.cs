369f95ea48b169485b71e6577e61d0cd04e87d43
Jéssica Bandeira Gomes <jessica.gomes@al.infnet.edu.br>
2025-03-29 01:22:32 -0300

    AT

---
+++ Exercicio07/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+class ContaBancaria
+{
+    public string Titular { get; set; } // Publico
+    private decimal _saldo; // Privado
+
+    public void Depositar(decimal valor) // Deposita um valor na conta, desde que seja positivo.
+    {
+        if (valor <= 0)
+        {
+            Console.WriteLine("\nO valor do depósito deve ser positivo!");
+        }
+        else
+        {
+            _saldo += valor;
+            Console.WriteLine($"\nDepósito de R$ {valor:F2} realizado com sucesso!");
+        }
+    }
+
+    public void Sacar(decimal valor) // Saca um valor da conta, se houver saldo suficiente.
+    {
+        if (valor <= 0)
+        {
+            Console.WriteLine("\nO valor do saque deve ser positivo!");
+        }
+        else if (valor > _saldo)
+        {
+            Console.WriteLine("\nSaldo insuficiente para realizar o saque!");
+        }
+        else
+        {
+            _saldo -= valor;
+            Console.WriteLine($"\nSaque de R$ {valor:F2} realizado com sucesso!");
+        }
+    }
+
+    public void ExibirSaldo() // Exibe o saldo atual formatado.
+    {
+        Console.WriteLine($"\nSaldo atual: R$ {_saldo:F2}");
+    }
+}
+
+class Program
+{
+    static void Main() // Método principal que gerencia o menu interativo do banco digital.
+    {
+        Console.WriteLine("=== BANCO DIGITAL ===");
+
+        // Criação da conta
+        ContaBancaria conta = new ContaBancaria();
+        Console.Write("\nDigite o nome do titular: ");
+        conta.Titular = Console.ReadLine();
+
+        // Menu interativo
+        while (true)
+        {
+            Console.WriteLine("\nOpções:");
+            Console.WriteLine("1 - Depositar");
+            Console.WriteLine("2 - Sacar");
+            Console.WriteLine("3 - Exibir Saldo");
+            Console.WriteLine("4 - Sair");
+            Console.Write("Escolha uma opção: ");
+
+            string opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1":
+                    Console.Write("\nDigite o valor do depósito: R$ ");
+                    if (decimal.TryParse(Console.ReadLine(), out decimal valorDeposito))
+                    {
+                        conta.Depositar(valorDeposito);
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nValor inválido!");
+                    }
+                    break;
+
+                case "2":
+                    Console.Write("\nDigite o valor do saque: R$ ");
+                    if (decimal.TryParse(Console.ReadLine(), out decimal valorSaque))
+                    {
+                        conta.Sacar(valorSaque);
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nValor inválido!");
+                    }
+                    break;
+
+                case "3":
+                    conta.ExibirSaldo();
+                    break;
+
+                case "4":
+                    Console.WriteLine("\nSaindo do sistema...");
+                    return;
+
+                default:
+                    Console.WriteLine("\nOpção inválida!");
+                    break;
+            }
+        }
+    }
+}
