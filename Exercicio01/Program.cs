b3431779e358356a77a34d7192da9c34b1ab27a6
Jéssica Bandeira Gomes <jessica.gomes@al.infnet.edu.br>
2025-03-29 00:55:49 -0300

    AT

---
+++ Exercicio01/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Olá, meu nome é Jéssica!");
+        Console.WriteLine("Nasci em 05/02/1996 e estou aprendendo C#!");
+    }
+}
